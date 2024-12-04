ba30b785a622bc9c27d47d6ad94c8115754c3b4c
CaioCasemiro <casemirocaio2@gmail.com>
2024-12-04 20:30:52 -0300

    código atualizado com novas funcionalidades

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import dominio.Musica;
 import dominio.Album;
 import dominio.Artista;
@@ -8,9 +9,13 @@ import java.awt.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        ArrayList<Artista> artistas = new ArrayList<>();
+
         // Criação de músicas
         Musica musica1 = new Musica("Call Out My Name", 238, "assets\\Call Out My Name.wav", "Sei lá KKKKKK");
         Musica musica2 = new Musica("Die For You", 260, "assets\\Die For You.wav", "Não sei KKKKKK");
+        Musica musica3 = new Musica("Lost", 234, "assets\\Lost.wav", "Não sei KKKKKK");
+        Musica musica4 = new Musica("Pink Matter", 268, "assets\\Pink Matter.wav", "Não sei KKKKKK");
 
         // Criando álbum e adicionando músicas no álbum
         Album album1 = new Album();
@@ -18,38 +23,74 @@ public class App {
         album1.addMusica(musica1);
         album1.addMusica(musica2);
 
+        Album album2 = new Album();
+        album2.setNome("Músicas Frank Ocean");
+        album2.addMusica(musica3);
+        album2.addMusica(musica4);
+
         // Criando o Artista e adicionando o álbum a ele
         Artista theweeknd = new Artista();
         theweeknd.setNome("The Weeknd");
         theweeknd.addAlbum(album1);
 
+        Artista FrankOcean = new Artista();
+        FrankOcean.setNome("Frank Ocean");
+        FrankOcean.addAlbum(album2);
+
+
+        artistas.add(theweeknd);
+        artistas.add(FrankOcean);
+
+
+        class State{
+        int currentArtistIndex = 0;
+        int currentAlbumIndex = 0;
+        Artista currentArtista = artistas.get(currentArtistIndex);
+        Album currentAlbum = currentArtista.getAlbuns().get(currentAlbumIndex);
+        }
+        
+
+        State state = new State();
+
+
         // Criando o AudioPlayer
         AudioPlayer player = new AudioPlayer(album1);
-        player.loadAudio(musica1.getArquivoAudio());
+        player.loadAudio(state.currentAlbum.getMusicas().get(0).getArquivoAudio());
 
         // Criação da janela principal (JFrame)
         JFrame frame = new JFrame("Vitrola Vea");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 150);
+        frame.setSize(300, 250);
         frame.setLocation(600, 330);
         frame.setLayout(new BorderLayout());
         
 
+        JPanel painelPrincipal = new JPanel();
+        painelPrincipal.setLayout(new BoxLayout(painelPrincipal, BoxLayout.Y_AXIS));
+
         // Criação do painel para os botões
-        JPanel panel = new JPanel();
+        JPanel painelDeControle = new JPanel();
         JButton prevButton = new JButton("<");
         JButton playStopButton = new JButton("Play");
         JButton nextButton = new JButton(">");
-        panel.add(prevButton);
-        panel.add(playStopButton);
-        panel.add(nextButton);
-
+        painelDeControle.add(prevButton);
+        painelDeControle.add(playStopButton);
+        painelDeControle.add(nextButton);
         
+        JPanel painelSwitch = new JPanel();
+        JButton switchArtistButton = new JButton("Trocar artista");
+        JButton switchAlbumButton = new JButton("Trocar album");
+        painelSwitch.add(switchAlbumButton);
+        painelSwitch.add(switchArtistButton);
+
+        painelPrincipal.add(painelDeControle);
+        painelPrincipal.add(painelSwitch);
+
         // Criação do JLabel para exibir o nome da música
-        JLabel trackLabel = new JLabel("Tocando agora: " + musica1.getNome(), SwingConstants.CENTER);
+        JLabel trackLabel = new JLabel("<html> | Artista: "+state.currentArtista.getNome()+"<br> | Álbum: "+state.currentAlbum.getNome()+"<br> | Música: "+player.getCurrentTrackName() + "</html>", SwingConstants.CENTER);
         frame.add(new JLabel(new ImageIcon("assets\\musica.png")), BorderLayout.NORTH);
         frame.add(trackLabel, BorderLayout.CENTER);
-        frame.add(panel, BorderLayout.SOUTH);
+        frame.add(painelPrincipal, BorderLayout.SOUTH);
         
         
 
@@ -67,13 +108,38 @@ public class App {
         // Ação botão Next
         nextButton.addActionListener(e -> {
             player.nextTrack();  
-            trackLabel.setText("Tocando agora: " + player.getCurrentTrackName());
+            trackLabel.setText("<html> | Artista: " + state.currentArtista.getNome() + "<br> | Álbum: " + state.currentAlbum.getNome() + "<br> | Música: " + player.getCurrentTrackName()+ "</html>");
         });
 
         // Ação botão Prev
         prevButton.addActionListener(e -> {
             player.prevTrack();
-            trackLabel.setText("Tocando agora: " + player.getCurrentTrackName());
+            trackLabel.setText("<html> | Artista: " + state.currentArtista.getNome() + "<br> | Álbum: " + state.currentAlbum.getNome() + "<br> | Música: " + player.getCurrentTrackName()+"</html>");
+        });
+
+        // Ação para trocar de artista
+        switchArtistButton.addActionListener(e ->{
+            player.stopAudio();
+            playStopButton.setText("Play");
+
+            state.currentArtistIndex = (state.currentArtistIndex + 1) % artistas.size();
+            state.currentArtista = artistas.get(state.currentArtistIndex);
+
+            state.currentAlbumIndex = 0;
+            state.currentAlbum = state.currentArtista.getAlbuns().get(state.currentAlbumIndex);
+
+        });
+
+
+        switchAlbumButton.addActionListener(e -> {
+            player.stopAudio();
+            playStopButton.setText("Play");
+
+            state.currentAlbumIndex = (state.currentAlbumIndex+1) % state.currentArtista.getAlbuns().size();
+            state.currentAlbum = state.currentArtista.getAlbuns().get(state.currentAlbumIndex);
+            player.loadAudio(state.currentAlbum.getMusicas().get(0).getArquivoAudio());
+
+            trackLabel.setText("<html> | Artista: " + state.currentArtista.getNome() + "<br> | Álbum: " + state.currentAlbum.getNome() + "<br> | Música: " + player.getCurrentTrackName() + "</html>");
         });
 
         // Exibindo a janela
